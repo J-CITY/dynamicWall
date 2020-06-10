02f183db7f15d141213d68b5910f7a608d9df8c5
Daniil Glushchenko <daniil.glushchenko1995@gmail.com>
2020-06-10 23:35:25 +1000

    Update web wall script

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #pip install pywin32
 #pip install PyQtWebEngine
-from PyQt5.QtCore import QUrl
+
+from PyQt5 import QtCore, QtGui 
+from PyQt5.QtGui import * 
+from PyQt5.QtCore import * 
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import QWebEngineSettings, QWebEngineView, QWebEnginePage
 import PyQt5
@@ -55,20 +58,25 @@ class Main(QWebEngineView):
             return []
 
     def initUI(self):
+        viewId = self.winId()
+
         workerW = self._findWindowHandles(windowClass='Progman')[0]
-        crypticParams = (0x52c, 0, 0, win32con.SMTO_NORMAL, 0x3e8, None)
-        user32.SendMessageTimeoutW(workerW, *crypticParams)
+        crypticParams = (0x52c, 0, 0, win32con.SMTO_NORMAL, 0x3e8)
+        win32gui.SendMessageTimeout(workerW, *crypticParams)
         win32gui.EnumWindows(EnumWindowsProc(), None)
         win32gui.ShowWindow(_WORKERW, win32con.SW_SHOW)
         
-        viewId = self.winId()
-
-        win32gui.GetWindowLong(viewId, win32con.GWL_STYLE)
-        win32gui.SetParent(viewId, workerW)
-        win32gui.SetWindowPos(viewId, win32con.HWND_TOP, 0,0,0,0, \
-            win32con.WS_EX_LEFT|win32con.WS_EX_LTRREADING|win32con.WS_EX_RIGHTSCROLLBAR|win32con.SWP_NOACTIVATE)
+        #win32gui.GetWindowLong(viewId, win32con.GWL_STYLE)
+        #win32gui.SetWindowPos(viewId, win32con.HWND_TOP, 0,0,0,0, \
+        #    win32con.WS_EX_LEFT|win32con.WS_EX_LTRREADING|win32con.WS_EX_RIGHTSCROLLBAR|win32con.SWP_NOACTIVATE)
         
-        self.load(QUrl('https://www.google.com/'))
+        self.setWindowFlags(Qt.Window|Qt.FramelessWindowHint|Qt.CoverWindow|Qt.WindowStaysOnBottomHint)
+        self.setWindowState(Qt.WindowNoState)
+        self.setFocusPolicy(Qt.NoFocus)
+
+        win32gui.SetParent(viewId, workerW)
+        
+        self.load(QUrl('C:/Users/Daniil (WORK)/Desktop/Wallpaper3/html/example/index.html'))
         self.showFullScreen()
         self.show()
         
